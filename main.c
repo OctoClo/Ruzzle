df677db66e6fd762e2e0979cf65282bf9a00df6b
OctoClo <octoclo@zoho.com>
2017-10-17 18:10:34 +0200

    Gitignore file and main.c : window creation

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <SDL2/SDL.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv)
+{
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+        printf("Error during SDL initialization : %s", SDL_GetError());
+
+    SDL_Window* window = SDL_CreateWindow("Ruzzle", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 500, 500, SDL_WINDOW_OPENGL);
+	if (!window)
+		printf("Error during window creation : %s", SDL_GetError());
+
+    printf("Hello world!\n");
+
+    getchar();
+
+    return 0;
+}
