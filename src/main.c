5f2f780c2f3e55773a3f60ffca9e999a321e31b7
OctoClo <octoclo@zoho.com>
2017-10-24 16:08:41 +0200

    Src directory

--- src/main.c
+++ src/main.c
@@ -6,11 +6,11 @@
 int main(int argc, char** argv)
 {
     if (SDL_Init(SDL_INIT_VIDEO) != 0)
-        printf("Error during SDL initialization : %s", SDL_GetError());
+        printf("Error during SDL initialization : %s\n", SDL_GetError());
 
     SDL_Window* window = SDL_CreateWindow("Ruzzle", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 500, 500, SDL_WINDOW_OPENGL);
 	if (!window)
-		printf("Error during window creation : %s", SDL_GetError());
+		printf("Error during window creation : %s\n", SDL_GetError());
 
     printf("Hello world!\n");
 
