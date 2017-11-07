4173abb811d84834ba7741e28e6268f7bd546ff1
OctoClo <octoclo@zoho.com>
2017-11-07 16:46:34 +0100

    Window display and basic event handling

--- src/main.c
+++ src/main.c
@@ -3,6 +3,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void handleEvents(SDL_Event*);
+void update();
+void render(SDL_Renderer*);
+
+enum Step { GAME, QUIT };
+enum Step step;
+
+SDL_Renderer* renderer;
+
 int main(int argc, char** argv)
 {
     if (SDL_Init(SDL_INIT_VIDEO) != 0)
@@ -12,9 +21,41 @@ int main(int argc, char** argv)
 	if (!window)
 		printf("Error during window creation : %s\n", SDL_GetError());
 
-    printf("Hello world!\n");
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (!renderer)
+        printf("Error during render creation : %s\n", SDL_GetError());
 
-    getchar();
+    SDL_Event event;
+    step = GAME;
 
-    return 0;
+    while (step != QUIT)
+    {
+        while (SDL_PollEvent(&event))
+            handleEvents(&event);
+        update();
+        render(renderer);
+    }
+
+    return EXIT_SUCCESS;
+}
+
+void handleEvents(SDL_Event* e)
+{
+    if (e->type == SDL_QUIT)
+        step = QUIT;
+}
+
+void update()
+{
+    // Mettre à jour des truuucs
+}
+
+void render(SDL_Renderer* renderer)
+{
+    SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
+    SDL_RenderClear(renderer);
+
+    // Afficher des truuuucs
+
+    SDL_RenderPresent(renderer);
 }
