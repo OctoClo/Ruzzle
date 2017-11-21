632f9f469edb81bbd23132ad9a836ff4e6d4cc9a
Marguerite <margue.francois@gmail.com>
2017-11-21 18:32:35 +0100

    Timer displays countdown

--- timer.c
+++ timer.c
@@ -10,15 +10,35 @@ int main(int argc, char** argv)
     if (SDL_Init(SDL_INIT_VIDEO) != 0)
         printf("Error during SDL initialization : %s\n", SDL_GetError());
 
-    int tempsPrecedent = 0, tempsActuel = 0;
+    int temps = 0, tempsActuel = 0, tempsDepart = 0;
+    tempsDepart = SDL_GetTicks();
 
+    while(temps<=120){
 
-    while(tempsActuel<=60000)
-    {
-        printf("temps: %d sec\n",tempsActuel/1000);
-        Sleep(1000);
-        tempsActuel = SDL_GetTicks();
+         //printf("depart : %d, temps : %d, tempsActuel : %d\n",tempsDepart/1000,temps,tempsActuel/1000);
+         if(temps==0){
+            printf("2:00\n");
+         }
+         else if(temps<=50){
+            printf("1:%d\n",60-temps);
+         }
+         else if(temps>60&&temps<=110){
+            printf("0:%d\n",120-temps);
+         }
+         else{
+            if(temps>50 && temps<60){
+                 printf("1:0%d\n",60-temps);
+            }
+            else{
+                printf("0:0%d\n",120-temps);
+            }
+
+         }
+         Sleep(1000);
+         tempsActuel = SDL_GetTicks();
+         temps=(tempsActuel/1000)-(tempsDepart/1000);
     }
+
 
 
 }
