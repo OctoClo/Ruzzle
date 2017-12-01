37eb196918f11182da2eebf440247428cee99415
Frank <fr13031997@gmail.com>
2017-12-01 18:18:46 +0100

    Save word from dico to Trie Tree and searching them into it

--- src/test.c
+++ src/test.c
@@ -5,20 +5,46 @@
 #include "LinkedLetter.h"
 #include "TrieNode.h"
 
+#define TAILLE_MAX 100
+
 
 int main(int argc, char** argv)
 {
 
 
+    FILE* dico = NULL;
+    char word[TAILLE_MAX];
+    int length;
+
+    //Ouverture du fichier dico
+    dico = fopen("src/file/dico.txt", "r");
+
+    //Déclaration de l'arbre préfixé
     TrieNode* root = initNode();
 
-    insertNode(root, "patate");
-    insertNode(root, "oui");
 
-    if(searchNode(root, "test")){
-        printf("'test' est present dans root !");
+    //Traitement du fichier dico
+    if(dico != NULL){
+
+
+        //Enregistrement des mots du dictionnaire dans l'arbre préfixée
+        while(fgets(word, TAILLE_MAX, dico) != NULL){
+            strtok(word, "\n");
+            insertNode(root, word);
+        }
+
+
+
+        fclose(dico);
+    }
+
+
+    //Vérification de présence du mot (Test)
+    if(searchNode(root, "jambon")){
+        printf("OUIIII");
+
     }else{
-        printf("'test' n'est pas present dans root !");
+        printf("NOOON");
     }
 
 
@@ -26,6 +52,11 @@ int main(int argc, char** argv)
 
 
 
+
+
+
+
+    return 0;
 
 
 }
