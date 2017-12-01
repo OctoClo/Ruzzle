b3de4de89449fda51a598d6a93d3f4a92bfa3247
Frank <fr13031997@gmail.com>
2017-12-01 16:38:41 +0100

    creating TrieNode struct and function (trie search tree)

--- src/test.c
+++ src/test.c
@@ -3,20 +3,26 @@
 #include "letter.h"
 #include "word.h"
 #include "LinkedLetter.h"
+#include "TrieNode.h"
 
 
 int main(int argc, char** argv)
 {
 
 
+    TrieNode* root = initNode();
+
+    insertNode(root, "patate");
+    insertNode(root, "oui");
+
+    if(searchNode(root, "test")){
+        printf("'test' est present dans root !");
+    }else{
+        printf("'test' n'est pas present dans root !");
+    }
 
 
 
-    Word* word = initWordNotEmpty("test");
-
-
-
-    printf("le mot : %s", getWord(word));
 
 
 
